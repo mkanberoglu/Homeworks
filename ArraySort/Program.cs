738f6886e64d43e53bdd47bfefd83ce46a361c7b
M.Mert Kanberoğlu <mkanberoglu@outlook.com>
2022-11-02 14:02:33 +0300

    02.11.2022 C#-Unity homework sorting an array

--- ArraySort/Program.cs
+++ ArraySort/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ArraySort 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            int[] arr = { 5, 18, 62, 12, 1, 3, 7};
+            int temp;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+
+        }
+    }
+}
