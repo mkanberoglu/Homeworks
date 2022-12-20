465d34a9b208895da4347082dd5b695d691f8e75
M.Mert Kanberoğlu <mkanberoglu@outlook.com>
2022-12-20 15:53:02 +0300

    MemoryGame

---
+++ MemoryGame/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox2);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox3);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox4);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox5);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox6);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox7);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox8);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(948, 462);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(240, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(477, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(714, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(3, 234);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(240, 234);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(477, 234);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(714, 234);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 462);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private Image[] shuffledImages;
+        private Dictionary<string, Image> imagesDict;
+        private List<Container> containers;
+        private ImageHelper imGen;
+        private Dictionary<int, int> pairs;
+        private int openedImages;
+        private List<int> pics;
+        private List<PictureBox> picsBoxList;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,166 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Image> listImage = new List<Image>();
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void RastgeleResimSec()
+        {
+            listImage.Clear();
+            List<Image> list = new List<Image>();
+
+            for (int i = 0; i < formImageList.Images.Count; i++)
+            {
+                list.Add(formImageList.Images[i]);
+            }
+            for (int i = 0; i < pictureBoxes.Count / 2; i++)
+            {
+                int index = random.Next(0, list.Count);
+
+                Image img = list[index];
+
+                listImage.Add(img);
+                listImage.Add(img);
+
+                list.Remove(img);
+            }
+            listImage = listImage.OrderBy(x => random.Next()).ToList();
+        }
+        private void SetPictureBoxes()
+        {
+            pictureBoxes.Clear();
+
+            foreach (var item in Controls)
+            {
+                if (item is PictureBox)
+                {
+                    PictureBox pictureBox = (PictureBox)item;
+
+                    pictureBox.Click += pictureBox_Click;
+
+                    pictureBox.Visible = true;
+
+                    pictureBox.Image = null;
+
+                    pictureBoxes.Add(pictureBox);
+                }
+            }
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SetPictureBoxes();
+            RastgeleResimSec();
+        }
+
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            if (listImage.Count <= 0) return;
+
+            if (timer1.Enabled) return;
+
+            PictureBox pictureBox = (PictureBox)sender;
+            int index = pictureBoxes.IndexOf(pictureBox);
+            pictureBox.Image = listImage[index];
+            int count = GetOpenedImage();
+
+            if (count >= 2)
+            {
+                int differentCount = GetOpenedDifferentCount();
+                timer1.Start();
+            }
+        }
+
+        private void RemoveOpenedImages()
+        {
+            List<PictureBox> removeds = new List<PictureBox>();
+
+            foreach (var item in pictureBoxes)
+            {
+                if (item.Image != null)
+                {
+                    item.Visible = false;
+
+                    listImage.Remove(item.Image);
+                    removeds.Add(item);
+                }
+            }
+
+            foreach (var item in removeds)
+            {
+                pictureBoxes.Remove(item);
+            }
+        }
+
+        private void CloseAllImages()
+        {
+            foreach (var item in pictureBoxes)
+            {
+                item.Image = null;
+            }
+        }
+
+        private int GetOpenedImage()
+        {
+            int count = 0;
+
+            foreach (var item in pictureBoxes)
+            {
+                if (item.Image != null)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int count = GetOpenedDifferentCount();
+
+            if (count == 1)
+            {
+                RemoveOpenedImages();
+            }
+            else
+            {
+                CloseAllImages();
+            }
+
+            timer1.Stop();
+
+            if (pictureBoxes.Count == 0)
+            {
+                MessageBox.Show("Kazandýnýz...");
+            }
+
+
+        }
+
+
+        private int GetOpenedDifferentCount()
+        {
+            List<Image> list = new List<Image>();
+
+
+            foreach (var item in pictureBoxes)
+            {
+                if (item.Image != null)
+                {
+                    if (!list.Contains(item.Image))
+                    {
+                        list.Add(item.Image);
+                    }
+                }
+            }
+
+            return list.Count;
+        }
+    }
+}
