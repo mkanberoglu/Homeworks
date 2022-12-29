62826c6b728883cb4eca5fb16b807f1273c557df
M.Mert Kanberoğlu <mkanberoglu@outlook.com>
2022-12-29 19:58:08 +0300

    Final

--- Xox/Form1.Designer.cs
+++ Xox/Form1.Designer.cs
@@ -42,79 +42,97 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(12, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 100);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(118, 57);
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(117, 57);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 100);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(224, 57);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 100);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button4.Location = new System.Drawing.Point(12, 163);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 100);
             this.button4.TabIndex = 5;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button5.Location = new System.Drawing.Point(118, 163);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 100);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button6.Location = new System.Drawing.Point(224, 163);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(100, 100);
             this.button6.TabIndex = 3;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button7.Location = new System.Drawing.Point(12, 269);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(100, 100);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button8.Location = new System.Drawing.Point(118, 269);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(100, 100);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button9.Location = new System.Drawing.Point(224, 269);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(100, 100);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // label1
             // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(147, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 40);
--- Xox/Form1.cs
+++ Xox/Form1.cs
@@ -2,14 +2,145 @@ namespace Xox
 {
     public partial class Form1 : Form
     {
+        private string player = "X";
         public Form1()
         {
             InitializeComponent();
+            label1.Text = player;
+ 
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private int openCount = 0;
+        private void button_Click(object sender, EventArgs e)
         {
+            Button b = (Button)sender;
+            if(!string.IsNullOrEmpty(b.Text)) return;
+            b.Text = player;
+            openCount++;
 
+            if(player == "X")
+            {
+                player = "O";
+            }
+            else { player = "X"; }
+            label1.Text = player;
+            if(openCount >= 3)
+            {
+                CheckMatches();
+            }
+        }
+        private void CheckMatches()
+        {
+            //Birinci button
+            if(button1.Text == button4.Text && button4.Text == button7.Text && !string.IsNullOrEmpty(button1.Text) && !string.IsNullOrEmpty(button4.Text) && !string.IsNullOrEmpty(button7.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if (button1.Text == button2.Text && button2.Text == button3.Text && !string.IsNullOrEmpty(button1.Text) && !string.IsNullOrEmpty(button2.Text) && !string.IsNullOrEmpty(button3.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && !string.IsNullOrEmpty(button1.Text) && !string.IsNullOrEmpty(button5.Text) && !string.IsNullOrEmpty(button9.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            //Ýkinci button
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && !string.IsNullOrEmpty(button2.Text) && !string.IsNullOrEmpty(button5.Text) && !string.IsNullOrEmpty(button8.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            //Üçüncü button
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && !string.IsNullOrEmpty(button3.Text) && !string.IsNullOrEmpty(button6.Text) && !string.IsNullOrEmpty(button9.Text) )
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && !string.IsNullOrEmpty(button3.Text) && !string.IsNullOrEmpty(button5.Text) && !string.IsNullOrEmpty(button7.Text) )
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            //Dördüncü button
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && !string.IsNullOrEmpty(button4.Text) && !string.IsNullOrEmpty(button5.Text) && !string.IsNullOrEmpty(button6.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            //Beþinci button
+            //Altýncý button
+            //Yedinci button
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && !string.IsNullOrEmpty(button7.Text) && !string.IsNullOrEmpty(button8.Text) && !string.IsNullOrEmpty(button9.Text) )
+            {
+                DialogResult dialogResult = MessageBox.Show($"{button1.Text} kazandý oyun bitti ... \n\rTekrar oynamak ister misiniz ?", "Game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
         }
     }
 }
